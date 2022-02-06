93e367d41940a7ccd7f2a9a094035eb0621984f0
zeroto99 <myv_01@naver.com>
2022-02-06 23:57:03 +0900

    :zap: showModal() 추가

--- m-modal/script.js
+++ m-modal/script.js
@@ -1,20 +1,23 @@
+const deleteBtn = document.querySelector('.delete');
 const modalBg = document.querySelector('.modal-background');
 const modal = document.querySelector('.modal');
 const cancelBtn = document.querySelector('.cancel-btn');
 
-function modalNone() {
+function showModal() {
+  modalBg.style.display = 'block';
+  modal.style.display = 'block';
+}
+
+function hideModal(e) {
+  e.preventDefault();
   modalBg.style.display = 'none';
   modal.style.display = 'none';
 }
 
-cancelBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  modalNone();
-});
+deleteBtn.addEventListener('click', showModal);
 
-modalBg.addEventListener('click', (e) => {
-  e.preventDefault();
-  modalNone();
-});
+cancelBtn.addEventListener('click', hideModal);
+
+modalBg.addEventListener('click', hideModal);
 
 
